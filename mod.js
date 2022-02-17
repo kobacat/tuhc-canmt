c432415d07dcf47295d792b2c4cf7571d79efe7d
koba <jaspycatty@gmail.com>
2022-02-17 12:59:04 -0500

    act 4 update

--- mod.js
+++ mod.js
@@ -27,17 +27,43 @@ module.exports = {
     <p>i dunno dude, it's, like, ur choice, you know?</p>`,
 
     routes: {
-        'assets://storyfiles/hs2/13294/13294.mp3': './music/13294/13294.mp3', // Megalovania
-        'assets://storyfiles/hs2/03294/03294.mp3': './music/03294/03294.mp3', // Megalovania
+		// ACT 1
         'assets://storyfiles/hs2/00137/00137.mp3': './music/00137/00137.mp3', // Sburban Jungle (Brief Mix)
         'assets://storyfiles/hs2/00222/00222.mp3': './music/00222/00222.mp3', // Aggrieve (Violin Refrain)
+		'assets://storyfiles/hs2/00246/00246.mp3': './music/00246/00246.mp3', // Sburban Cascantdown
         'assets://storyfiles/hs2/10246/10246.mp3': './music/10246/10246.mp3', // Sburban Cascantdown
+		// ACT 2
         'assets://storyfiles/hs2/00253/afterthought-showtime_piano.mp3': './music/00253/afterthought-showtime_piano.mp3', // Showtime (Piano Refrain)
         'assets://storyfiles/hs2/00644/00644.mp3': './music/00644/00644.mp3', // John Sleeps / Skaian Magicant
         'assets://storyfiles/hs2/00651/00651.mp3': './music/00651/00651.mp3', // John Sleeps / Skaian Magicant
         'assets://storyfiles/hs2/00665/00665.mp3': './music/00665/00665.mp3', // Upward Movement (Dave Owns)
         'assets://storyfiles/hs2/00755/00755.mp3': './music/00755/00755.mp3', // Sburban Reversal (Original)
         'assets://storyfiles/hs2/00757/00757.mp3': './music/00757/00757.mp3', // Explore
+		// ACT 3
+		'assets://storyfiles/hs2/00833/00833.mp3': './music/00833/00833.mp3', // Dead Shuffle
+		'assets://storyfiles/hs2/pony/pony.mp3': './music/pony/pony.mp3', // Pony Chorale
+		'assets://storyfiles/hs2/00948/00948.mp3': './music/00948/00948.mp3', // Revelawesome
+		'assets://storyfiles/hs2/00979/00979.mp3': './music/00979/00979.mp3', // Harlequin
+		'assets://storyfiles/hs2/00980/00980_1.mp3': './music/00980/00980_1.mp3', // An Unbreakable Union
+		'assets://storyfiles/hs2/00980/00980_2.mp3': './music/00980/00980_2.mp3', // Carefree Victory
+		'assets://storyfiles/hs2/01026/01026.mp3': './music/01026/01026.mp3', // The Beginning of Something Really Excellent
+		'assets://storyfiles/hs2/01070/01070.mp3': './music/01070/01070.mp3', // Versus
+		'assets://storyfiles/hs2/01073/01073.mp3': './music/01073/01073.mp3', // Ballad of Awakening
+		'assets://storyfiles/hs2/01149/01149.mp3': './music/01149/01149.mp3', // Sburban Jungle
+		'assets://storyfiles/hs2/01267/01267.mp3': './music/01267/01267.mp3', // Three in the Morning
+		// ACT 4
+		'assets://storyfiles/hs2/01407/01407.mp3': './music/01407/01407.mp3', // Endless Climb
+		'assets://storyfiles/hs2/01641/01641.mp3': './music/01641/01641.mp3', // Atomyk Ebonpyre
+		'assets://storyfiles/hs2/01668/01668.mp3': './music/01668/01668.mp3', // Black
+		'assets://storyfiles/hs2/01801/01801.mp3': './music/01801/01801.mp3', // Skaian Skirmish
+		'assets://storyfiles/hs2/01931/01931.mp3': './music/01931/01931.mp3', // How Do I Live (Bunny Back in the Box Version)
+		'assets://storyfiles/hs2/11931/11931.mp3': './music/11931/11931.mp3', // How Do I Live (Bunny Back in the Box Version)
+		'assets://storyfiles/hs2/01940/01940.mp3': './music/01940/01940.mp3', // Descend
+		// ACT 5
+		'assets://storyfiles/hs2/13294/13294.mp3': './music/13294/13294.mp3', // Megalovania
+        'assets://storyfiles/hs2/03294/03294.mp3': './music/03294/03294.mp3', // Megalovania
+		
+		
     },
 
     trees: {
